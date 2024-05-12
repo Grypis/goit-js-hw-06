eb2fcb8d4c957f0580882a273e28f34aa1d2c2b9
Grypis <grypisnecros@gmail.com>
2024-05-12 20:47:04 +0300

    2 add

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+'use strict';
+class Storage {
+  #items;
+  constructor(initialItems = []) {
+    this.#items = initialItems;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.#items.indexOf(itemToRemove) !== -1) {
+      this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
